5db88e80cddf97f7f47b225bda6876224f08a2e6
ITD <a@a.a>
2024-01-30 09:26:08 +0100

    ea4wta

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const gomb = document.querySelector('#gomb')
-const emailinput = document.querySelector('#email')
-const result = gomb.addEventListener('click', function () {
-  console.log(emailinput.value)
-})
+const gomb = document.querySelector("#gomb");
+const emailinput = document.querySelector("#email");
 
-const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-if (emailinput.value.match(mailformat)) {
-}
-console.log('aaaaaaaaa')
+gomb.addEventListener("click", function () {
+  console.log(emailinput.validity.patternMismatch);
+  if (!emailinput.validity.patternMismatch) {
+    console.log("valid");
+  } else {
+    console.log("invalid");
+  }
+});
